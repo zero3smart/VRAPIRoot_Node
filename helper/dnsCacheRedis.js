d2f38961d1317c073be4253013fdaaf130e7402c
billionsoftech <fivewisdomzero@gmail.com>
2018-03-29 22:06:24 -0400

    removing log as it will overflow any log file while inserting

--- helper/dnsCacheRedis.js
+++ helper/dnsCacheRedis.js
@@ -2,30 +2,47 @@
  * Created by titu on 12/5/16.
  */
 const redis = require('redis');
+const redisClient = require('./redis').redisClient;
 const promise = require('bluebird');
+const dns = promise.promisifyAll(require("dns"));
+const dnscacher = require('dnscache');
 
-promise.promisifyAll(redis.RedisClient.prototype);
-promise.promisifyAll(redis.Multi.prototype);
-
-let initializeRedis = () => {
-    let client = redis.createClient();
-
-    return new promise(function (resolve, reject) {
-        client.on('connect', function () {
-            module.exports.redisClient = client;
-            console.log('Redis connected successfully.');
-            resolve(client);
-        });
-
-        client.on('error', function (error) {
-            console.log('Error in connecting with Redis: ');
-            console.log(error);
-            reject(error);
-        });
+let initialize = () => {
+    let dnsCache = dnscacher({
+        "enable": true,
+        "ttl": 2592000,
+        "cachesize": 5000000,//5000k
+        "cache": _dnsCacheWithRedis
     });
+    module.exports.dnsCache = dnsCache;
+
+    return dnsCache;
+};
+
+let _dnsCacheWithRedis = function dnsCacheRedis(config) {
+
+    var redisKey = 'dnscache:';
+    var redisClient = require('./redis').redisClient;
+
+    config.ttl = config.ttl || 2592000 // Cache for 30 days
+
+    this.set = function (key, value, callback) {
+        redisClient.set(redisKey + key, JSON.stringify(value), 'NX', 'EX', config.ttl, function (err) {
+            if (callback) {
+                callback(err, value);
+            }
+        });
+    };
+
+    this.get = function (key, callback) {
+        redisClient.get(redisKey + key, function (err, item) {
+            if (callback) {
+                callback(err, JSON.parse(item));
+            }
+        });
+    };
 };
 
 module.exports = {
-    initialize: initializeRedis,
-    //redis.client
+    initialize: initialize
 };
